99308dc2b5feab81e4d765a7f81955b27ae9feb0
Sosa99 <57610056+Sosa99@users.noreply.github.com>
2020-11-24 15:19:21 +0800

    Delete TU06 .py

--- TU06 .py
+++
@@ -1,8 +0,0 @@
-TwinTowers = 452
-MyCityTallestBuilding = int(input("What's the height of your tallest building in metres?:"))
-if TwinTowers > MyCityTallestBuilding:
-    print("The KL Twin Towers is taller than your building!")
-elif TwinTowers == MyCityTallestBuilding:
-   print("You must live in KL too!")
-else: 
-    print("Damn, my city's tallest building is shorter than yours...")
