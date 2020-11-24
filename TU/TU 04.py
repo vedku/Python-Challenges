8a208847f64d89d4db1d400331943505dd5178e8
Sosa99 <57610056+Sosa99@users.noreply.github.com>
2020-11-24 14:47:31 +0800

    Create TU 04.py

---
+++ TU/TU 04.py
@@ -0,0 +1,5 @@
+test = input("Is it raining? yes/no:")
+if test in ["yes","yeah","ye","mhmm","yh","yuh","ya","yah","yup","yea"]:
+    print("Bloody hell, no football today!")
+if test in ["no","na","nope"]:  
+    print("Get ready to get rekt noob")
