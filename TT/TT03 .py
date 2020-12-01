68e572e95f3310408b4211a5d70d63b646d42ad5
Sosa99 <57610056+Sosa99@users.noreply.github.com>
2020-12-01 14:34:31 +0800

    Create TT03 .py

---
+++ TT/TT03 .py
@@ -0,0 +1,13 @@
+import turtle
+turtle.hideturtle()
+turtle.bgcolor('yellow')
+ws = turtle.Screen()
+ 
+geekyTurtle = turtle.Turtle()
+ 
+for i in range(8):
+   
+    geekyTurtle.forward(100)
+     
+   
+    geekyTurtle.left(45)
