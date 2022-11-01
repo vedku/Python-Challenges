4b39ceb0a6c993e92c3403a43c73af5de7d9516f
vedku <57610056+vedku@users.noreply.github.com>
2022-11-01 14:08:18 +0800

    Create AS226.py

---
+++ AS/AS226.py
@@ -0,0 +1,36 @@
+#no worky yet
+I = 1
+II = 2
+III = 3
+IV = 4
+V = 5
+VI = 6
+VII = 7
+VIII = 8
+IX = 9
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+denary_number_to_convert = 0
+roman_number_to_convert = []
+
+def denary2roman(denary):
+    denary = [char for char in denary]
+    print(denary)
+denary2roman(input("What denary number do you want to convert to roman numerals?:"))
+
+monkey = denary2roman()
+print(monkey/2)
+
+
+
+
+#user_choice = input("If you want to convert from denary to roman numerals, enter x \nIf you want to convert from roman numerals to denary, enter y.\nInput here:")
+#if user_choice in ("y"):
+    #roman_number_to_convert = input("What number do you want to convert from denary to roman numerals?:")
+    #print(roman_number_to_convert)
+
+
+
