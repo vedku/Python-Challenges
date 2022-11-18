46583701c1bf140b802e627626ce97aebfa581cb
vedku <57610056+vedku@users.noreply.github.com>
2022-11-18 18:24:02 +0800

    Update A2259_binomialtheorem.py

--- A2/A2259_binomialtheorem.py
+++ A2/A2259_binomialtheorem.py
@@ -5,9 +5,11 @@ def expand(co_a, co_b, n):
         print(co_a,"a", "+", co_b,"b")
     elif n > 1:
         for i in range(n):
-            ans = co_a*co_a,"a", "+", co_a*co_b,"b", "+", co_b*co_b
+            a_value = co_a**2
+            b_value = 2*(co_a*co_b)
+            c_value = co_b**2
             #print("the amount of times this is printed is the amount of times it needs to be multiplied by itself")
-            print(ans)
+            print(a_value, b_value, c_value)
 
 print("(a+b)^n")
 co_a = int(input("What is the coefficient of a?:"))
