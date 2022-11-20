6c5a11a5e4e247eea3f99bf1a40276d5d83a4cc3
vedku <57610056+vedku@users.noreply.github.com>
2022-11-20 20:51:16 +0800

    Create AS73_randompasswordmaker.py

---
+++ AS/AS73_randompasswordmaker.py
@@ -0,0 +1,7 @@
+usable_characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',"$", "#", "@", "^", "&",'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', ]
+password = ""
+import secrets
+for i in range(8): 
+    password += secrets.choice(usable_characters)
+print(password)
+#changed to secrets because it's more secure apparently. 
