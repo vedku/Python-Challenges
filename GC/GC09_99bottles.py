86b1e22bfcc40e6ae809c73bd474522007e1e0ed
vedku <57610056+vedku@users.noreply.github.com>
2022-12-12 18:18:27 +0800

    Create GC09_99bottles.py

---
+++ GC/GC09_99bottles.py
@@ -0,0 +1,7 @@
+num_bottles = 99
+while num_bottles > 0:
+     print(str(num_bottles) + " bottles of cola on the wall")
+     print(str(num_bottles) + " bottles of cola")
+     print("take one down, pass it around")
+     print(str(num_bottles - 1) + " bottles of cola on the wall")
+     num_bottles = num_bottles - 1
