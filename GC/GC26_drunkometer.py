88eee92bb465c466699d85c174e3a208e3820ad2
vedku <57610056+vedku@users.noreply.github.com>
2022-12-12 19:28:23 +0800

    Create GC26_drunkometer.py

---
+++ GC/GC26_drunkometer.py
@@ -0,0 +1,9 @@
+x = ("I am normal and can be trusted to operate heavy machinery")
+print(x)
+print("type out this statement twice")
+y = input()
+z = input()
+if y and z == x:
+    print("you are fit to work")
+else:
+    print("sober up")
