960d1139cbf91f2d97ff6d7868711b128f7ccf7f
vedku <57610056+vedku@users.noreply.github.com>
2022-12-13 17:28:07 +0800

    Create GC27_fizzbuzz.py

---
+++ GC/GC27_fizzbuzz.py
@@ -0,0 +1,11 @@
+x = 1
+for x in range(21):
+    if x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    elif x % 15 == 0:
+        print("FizzBuzz")
+    else:
+        print(x)
+    x += 1
