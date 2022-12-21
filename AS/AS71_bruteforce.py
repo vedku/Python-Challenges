2f196677564d9949e30049045a4ebc63d7c09f62
vedku <57610056+vedku@users.noreply.github.com>
2022-12-21 21:59:16 +0800

    Update AS71_bruteforce.py

--- AS/AS71_bruteforce.py
+++ AS/AS71_bruteforce.py
@@ -3,11 +3,13 @@ password = ""
 copycat = ""
 length = int(input("How long do u want your password to be?:"))
 import random
-for i in range(length):
-    password += random.choice(usable_characters)
-for i in range(length):
-    copycat += random.choice(usable_characters)
-if password == copycat:
-    print(password)
-    print(copycat)
-    print("they match!!!1!!!1!")
+while True:
+    for i in range(length):
+        password += random.choice(usable_characters)
+    for i in range(length):
+        copycat += random.choice(usable_characters)
+    if password == copycat:
+        print(password)
+        print(copycat)
+        print("they match!!!1!!!1!")
+        break
