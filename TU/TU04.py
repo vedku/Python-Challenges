f143e1a4d60b853b1fc7448dd894af24a3a253d6
vedku <57610056+vedku@users.noreply.github.com>
2022-12-21 15:10:55 +0800

    Create TU04.py

---
+++ TU/TU04.py
@@ -0,0 +1,4 @@
+PersonAge = int(input('When was the person born?'))
+MyAge = int(input('When were you born?'))
+AgeDifference = MyAge - PersonAge
+print("The person is",AgeDifference,"years older than you")
