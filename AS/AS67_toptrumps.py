3090ab4aef78551b5f3d87b2cf32655379fe0e19
Ved Kuwadekar <57610056+vedku@users.noreply.github.com>
2023-11-09 08:37:10 +0800

    Create AS67_toptrumps.py

---
+++ AS/AS67_toptrumps.py
@@ -0,0 +1,9 @@
+import random
+player1= random.randint(1,100)
+player2= random.randint(1,100)
+print("1", player1)
+print("2", player2)
+if player2>player1:
+    print("player 2 wins")
+else:
+    print("player 1 wins")
